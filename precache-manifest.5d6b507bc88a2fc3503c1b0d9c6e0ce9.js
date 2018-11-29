7e99138984c15140bb81854cd9210d943b27458f
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-29 01:45:57 +0000

    Updates

---
+++ precache-manifest.5d6b507bc88a2fc3503c1b0d9c6e0ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "a4d972641b40aa049df4",
+    "url": "/react-portfolio/static/js/main.a4d97264.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-portfolio/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "a4d972641b40aa049df4",
+    "url": "/react-portfolio/static/css/main.88e0b1dc.chunk.css"
+  },
+  {
+    "revision": "d2a10a524601046c6b7f9b8bae01e48c",
+    "url": "/react-portfolio/index.html"
+  }
+];
