7cc7804141cc37c257defadad6d22d36f9085ea7
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-29 13:33:49 +0000

    Updates

---
+++ precache-manifest.f973c36d285c7f71ea3fdeaaa4aebd92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f87e7f74f1230b069f799820ff670e0",
+    "url": "/static/media/about.9f87e7f7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a2220e6f8a82145219d",
+    "url": "/static/js/main.2a2220e6.chunk.js"
+  },
+  {
+    "revision": "9a095b23cbeaea74c02b",
+    "url": "/static/js/1.9a095b23.chunk.js"
+  },
+  {
+    "revision": "2a2220e6f8a82145219d",
+    "url": "/static/css/main.97fcf718.chunk.css"
+  },
+  {
+    "revision": "e42ed97c1666bd7e94e025d0c01c5093",
+    "url": "/index.html"
+  }
+];
