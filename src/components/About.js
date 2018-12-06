4a5a103ae556e666434b7da776773a40dcfdc4a0
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-06 13:30:22 +0000

    correcting grammar of about page, adding color too!

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ class About extends React.Component{
     render() {
         return (
             <div className="About">
-                <p>HELLLLOOOO</p>
+                <p>DM me with <span>jobs</span>: yujinost@gmail.com</p>
             </div>
         );
     }
