6d3a41c4242c4e2b0a73ed6d1f325e38357295b5
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-06 13:31:48 +0000

    fixing routing with hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
 import { Error } from "./components/Error";
 import { Navigation } from "./components/Navigation";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
