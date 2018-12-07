1c0c2766051143f79a7cb6ff70ed4e18e6fafa38
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-07 13:38:16 +0000

    fixed css positioning of home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import MyFace from "../images/about.jpg";
 import { Navigation } from "./Navigation";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>You've Bumped into this guy</h1>
-        <img src={MyFace} alt={"MyFace"} className="myface" />
-      </header>
+      <body class="home">
+      <h1>You've Bumped into this guy</h1>
+      <img src={MyFace} alt={"MyFace"} className="myface" />
       <h1>What do you do next?</h1>
-      <h1>      <Navigation /></h1>
+      <h1><Navigation /></h1>
+      </body>
     </div>
   );
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./About.css";
 import { NavLink } from "react-router-dom";
+import "./Navigation.css"
 
 class Navigation extends React.Component{
     render() {
         return (
             <div className="Navigation">
-                <NavLink to="/">1. (*Stay silent*) <span>Stare</span> into his eyes.</NavLink><br/>
-                <NavLink to="/Projects">2. "You're a Developer right? <br/>What have you <span>work</span>ed on recently?"</NavLink><br/>
-                <NavLink to="/About">3. "Tell me a bit <span>about</span> yourself?"</NavLink>
+                <NavLink to="/">1. <span>Stare</span> into his eyes.</NavLink><br/>
+                <NavLink to="/Projects">2. <i>"You're a Developer right? <br/>What have you <span>work</span>ed on recently?"</i></NavLink><br/>
+                <NavLink to="/About">3. <i>"Tell me a bit <span>about</span> yourself?"</i></NavLink>
             </div>
         );
     }
