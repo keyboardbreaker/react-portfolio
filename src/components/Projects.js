6848b7c78a1fa1e3c52fa0ac423e27e6b07ac86d
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-14 14:55:54 +0000

    added project to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./Projects.css";
+import { NavLink } from "react-router-dom";
 
 class Projects extends React.Component{
     render() {
         return (
             <div className="Projects">
+                <h1><i>Well I've worked on:</i></h1>
+                <div className='borderBox'> 
+                    <h1><i class="fab fa-github" aria-hidden="true"></i><span>COOK</span> WITH <a 
+                        href="https://github.com/keyboardbreaker/react-portfolio" 
+                        alt="">SEASONAL CUISINE</a></h1>
+                </div> 
                 <div className='borderBox'> 
                     <h1>KODIRI IS <span>LOVE</span>, <a 
                         href="https://github.com/keyboardbreaker/kodflix" 
                         alt="">KODFLIX IS LIFE</a></h1>
                 </div>
                 <div className='borderBox'> 
-                    <h1><i class="fab fa-github" aria-hidden="true"></i>ONE DAY <a 
+                    <h1><i class="fab fa-github" aria-hidden="true"></i>MY <a 
                         href="https://github.com/keyboardbreaker/react-portfolio" 
                         alt="">PORTFOLIO</a></h1>
                 </div> 
+                <h1><NavLink to="/#">"Wow! Please tell me <span>more."</span></NavLink><br/></h1>
             </div>
         );
     }
