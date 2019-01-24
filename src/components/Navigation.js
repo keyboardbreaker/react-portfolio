e9603119cca43dca8c3c015096ded6b32d4beca5
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-24 11:01:30 +0000

    Adding contact details to home

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ class Navigation extends React.Component{
     render() {
         return (
             <div className="Navigation">
-                <NavLink to="/Projects">1. <i>"You're a Fullstack JavaScript <span>developer</span> right?"</i></NavLink><br/>
+                <NavLink to="/Projects">1. <i>"You're a <span>developer</span> right?"</i></NavLink><br/>
                 <NavLink to="/About">2. <i>"Tell me <span>about</span> yourself?"</i></NavLink><br/>
             </div>
         );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Projects.css";
 import { NavLink } from "react-router-dom";
+import latestFlix from "../images/latest-flix.png";
+import seasonal from "../images/seasonal-cuisine.png";
+import portfolio from "../images/portfolio.png";
 import "./Navigation.css"
 
 class Projects extends React.Component{
@@ -12,16 +15,19 @@ class Projects extends React.Component{
                     <h1><a 
                         href="https://github.com/keyboardbreaker/seasonal-cuisine" 
                         alt="">SEASONAL CUISINE</a></h1>
+                    <img src={seasonal} alt={"proj1"} className="projects" />
                 </div> 
                 <div className='borderBox'> 
                     <h1><a 
                         href="https://github.com/keyboardbreaker/kodflix" 
                         alt="">LATESTFLIX</a></h1>
+                    <img src={latestFlix} alt={"proj2"} className="projects" />
                 </div>
                 <div className='borderBox'> 
                     <h1><a 
                         href="https://github.com/keyboardbreaker/react-portfolio" 
                         alt="">PORTFOLIO</a></h1>
+                    <img src={portfolio} alt={"proj3"} className="projects" />
                 </div> 
                 <h1><NavLink to="/#">Return <span>Home</span>?</NavLink><br/></h1>
             </div>
