bc6fdc3dec604ab7aa31000f652d120b8c3fff07
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-25 13:59:00 +0000

    adding redirect to home from error path page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './Error.css';
+import { NavLink } from "react-router-dom";
 
 class Error extends React.Component{
     render() {
         return (
             <div className="Error">
                 <h1>Path Does not Exist</h1>
+                <h1>
+                    <NavLink to="/#">
+                    Return <span>Home</span>?
+                    </NavLink>
+                    <br />
+                </h1>
             </div>
         );
     }
