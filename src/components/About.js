ebce8caef9e6ca9b7c2fd5c1335fed22093b7260
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-25 12:37:56 +0000

    componentised my contact details component

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./About.css";
+import { ContactDetails } from "./ContactDetails";
 
 class About extends React.Component{
     render() {
         return (
             <div className="About wrapper">
-                <div className="content home ">
-                <p>DM me with <span>jobs</span>: yujinost@gmail.com</p>
+                <div className="content home App">
+                <p>I am enthusiastic, motivated and capable of working effectively 
+                    in teams and independently as demonstrated by my contributions 
+                    to the <a
+                            href="https://kodiri.github.io/seasonal-cuisine/#/"
+                            alt=""
+                           ><span>Seasonal Cuisine</span></a> App</p>
+                
+                <p>Find out more by contacting me</p>
+                <ContactDetails/>
                 <h1><NavLink to="/#">Return <span>Home</span>?</NavLink><br/></h1>
                 </div>
             </div>
