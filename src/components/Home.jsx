a194781b7bba431b44421fee8344233337634656
keyboardbreaker <yujin_1991@hotmail.com>
2025-07-30 01:30:51 +0100

    update(ui): converting class components to functional components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,31 +4,21 @@ import { Navigation } from "./Navigation";
 import { ContactDetails } from "./ContactDetails";
 import "./Home.css";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      message: "What do you do next?"
-    };
-  }
-  
-  render() {
-    return (
-      <div>
-        <div className="App wrapper">
-          <div className="home content">
-            <h1>You've Bumped into this guy</h1>
-            <img src={MyFace} alt={"MyFace"} className="myface" />
-            <p>Something about him is <span>alluring</span>, you approach him.</p>
-            <h1>{this.state.message}</h1>
-            <h1><Navigation /></h1>
-            Contact Details:
-            <ContactDetails />
-          </div>
+function Home() {
+  return (
+    <div>
+      <div className="App wrapper">
+        <div className="home content">
+          <h1>You've Bumped into this guy</h1>
+          <img src={MyFace} alt={"MyFace"} className="myface" />
+          <p>Something about him is <span>alluring</span>, you approach him.</p>
+          <h1>{'What do you do next?'}</h1>
+          <h1><Navigation /></h1>
+          Contact Details:
+          <ContactDetails />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export { Home };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,15 +3,13 @@ import "./About.css";
 import { NavLink } from "react-router-dom";
 import "./Navigation.css"
 
-class Navigation extends React.Component{
-    render() {
-        return (
-            <div className="Navigation">
-                <NavLink to="/Projects">1. <i>"You're a <span>developer</span> right?"</i></NavLink><br/>
-                <NavLink to="/About">2. <i>"Tell me <span>about</span> yourself?"</i></NavLink><br/>
-            </div>
-        );
-    }
+function Navigation() {
+    return (
+        <div className="Navigation">
+            <NavLink to="/Projects">1. <i>"You're a <span>developer</span> right?"</i></NavLink><br/>
+            <NavLink to="/About">2. <i>"Tell me <span>about</span> yourself?"</i></NavLink><br/>
+        </div>
+    );
 }
 
 export {Navigation}
