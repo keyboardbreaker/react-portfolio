73114dcc2f6a31c7282edc131a2f2c90d1433b09
keyboardbreaker <yujin_1991@hotmail.com>
2025-07-30 01:24:05 +0100

    fix(ui): centering and wrapping about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,16 +8,18 @@ class About extends React.Component{
         return (
             <div className="About wrapper">
                 <div className="content home App">
-                <p>I am enthusiastic, motivated and capable of working effectively 
-                    in teams and independently as demonstrated by my contributions 
-                    to the <a
-                            href="https://kodiri.github.io/seasonal-cuisine/#/"
-                            alt=""
-                           ><span>Seasonal Cuisine</span></a> App</p>
-                
-                <p>Find out more by contacting me</p>
-                <ContactDetails/>
-                <h1><NavLink to="/#">Return <span>Home</span>?</NavLink><br/></h1>
+                    <div className="information">
+                        <p>I am enthusiastic, motivated and capable of working effectively 
+                            in teams and independently as demonstrated by my contributions 
+                            to the <a
+                                    href="https://kodiri.github.io/seasonal-cuisine/#/"
+                                    alt=""
+                                ><span>Seasonal Cuisine</span></a> App</p>
+                        
+                        <p>Find out more by contacting me</p>
+                        <ContactDetails/>
+                        <h1><NavLink to="/#">Return <span>Home</span>?</NavLink><br/></h1>
+                    </div>
                 </div>
             </div>
         );
